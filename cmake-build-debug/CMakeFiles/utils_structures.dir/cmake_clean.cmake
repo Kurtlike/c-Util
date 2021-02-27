f0182919d219acb5daf84d30f275f219a49e4a38
Kurtlike <gordonfriman2000@gmail.com>
2021-02-27 18:01:36 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/utils_structures.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utils_structures.dir/main.cpp.obj"
+  "libutils_structures.dll.a"
+  "utils_structures.exe"
+  "utils_structures.exe.manifest"
+  "utils_structures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utils_structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
